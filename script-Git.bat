9b6ca109178a9262c5a9e14a4777ce287db7cd7e
Jordan <mistert9412@gmail.com>
2019-11-27 17:56:05 +0100

    Amelioration du script

--- script-Git.bat
+++ script-Git.bat
@@ -1,5 +1,5 @@
 @echo off
-set PATH=%PATH%;D:\Donn√©es\Documents\Helha2\TI
+::set PATH=%PATH%;D:\Donn√©es\Documents\Helha2\TI
 git status
 color 0e
 
@@ -8,8 +8,8 @@ echo.
 echo.
 :menu
 
-echo       Make your choice (1-6)
 echo ------------------------------------
+echo       Make your choice (1-6)
 echo.
 echo 	[1] Pull
 echo 	[2] Push
@@ -29,7 +29,65 @@ set /p choix=
 	
 :pull
 
+echo.
+echo ------------------------------------
+echo.
+echo	[1] Pull current folder
+echo	[2] Pull All
+echo.
+echo ------------------------------------
+set /p choix3=
+	if %choix3% ==1 GOTO PullOne
+	if %choix3% ==2 GOTO PullAll
+	
+:pullOne
+
+set curentPath=%cd%
 git pull
+echo.
+echo.
+echo ------------------------------------
+echo	Pull finished successfully
+echo.
+echo		[1] Return to Menu
+echo 	[2] exit
+echo.
+echo ------------------------------------
+set /p choix2=
+	if %choix2% ==1 GOTO menu
+	if %choix2% ==2 GOTO exit
+	
+:pullAll
+
+echo.
+echo.
+echo pull Java Design Pattern
+echo.
+::set PATH=%PATH%;D:\Donn√©es\Documents\Helha2\TI
+::git pull
+::set path1=%path%;D:\DonnÈes\Documents\Helha2\Workspace Design patterns
+::git pull
+
+::set path2=%path%;D:\DonnÈes\Documents\Helha2\Workspace Cour Java
+::git pull
+
+cd D:\DonnÈes\Documents\Helha2\Workspace Design patterns
+echo %cd%
+cd D:\Donn√©es\Documents\Helha2\Workspace Cour Java
+echo %cd%
+
+echo.
+echo.
+echo ------------------------------------
+echo	Pull All finished successfully
+echo.
+echo		[1] Return to Menu
+echo 	[2] exit
+echo.
+echo ------------------------------------
+set /p choix2=
+	if %choix2% ==1 GOTO menu
+	if %choix2% ==2 GOTO exit
 
 :push
 
@@ -44,11 +102,13 @@ git push
 echo.
 echo.
 echo.
-echo.
-echo ------------------------------- 
+echo ------------------------------------
+echo	Push finished successfully
 echo.
 echo		[1] Return to Menu
 echo 	[2] exit
+echo.
+echo ------------------------------------
 set /p choix2=
 	if %choix2% ==1 GOTO menu
 	if %choix2% ==2 GOTO exit
@@ -65,14 +125,14 @@ git config --global http.proxy http://172.30.0.101:8080/
 
 echo.
 echo.
-echo ----------------------------
+echo ------------------------------------
 echo.
-echo Proxy successfully modified
+echo 	Proxy successfully modified
 echo. 
 echo		[1] Return to Menu
 echo 	[2] exit
 echo.
-echo ----------------------------
+echo ------------------------------------
 set /p choix2=
 	if %choix2% ==1 GOTO menu
 	if %choix2% ==2 GOTO exit
@@ -82,14 +142,14 @@ set /p choix2=
 git config --global --unset http.proxy
 
 echo.
-echo --------------------------
+echo ------------------------------------
 echo.
-echo Proxy reset succesfull
+echo 	Proxy reset succesfull
 echo.
 echo		[1] Return to Menu
 echo 	[2] exit
 echo.
-echo --------------------------
+echo ------------------------------------
 set /p choix2=
 	if %choix2% ==1 GOTO menu
 	if %choix2% ==2 GOTO exit
